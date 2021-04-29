89fbfddebc95f33fc18eb8a617a0675317bcf6b7
SurajSongara <SurajSongara>
2021-04-29 19:40:45 +0530

    commit2

--- RecommendationGuid/settings.py
+++ RecommendationGuid/settings.py
@@ -22,6 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SECRET_KEY']
+# SECRET_KEY= 'cgg(b7+a%es5hnnqcfqaz30yyu6a@^q(6ai9!uayn4@op@^wfr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
